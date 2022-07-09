4bb1c41871b9d1e126116c1ce43988c51f99ce01
MofijulHaque <mofijulhoque995@gmail.com>
2022-07-09 12:33:44 +0530

    09-07-2022

---
+++ 09-07-2022/problem_1/index.js
@@ -0,0 +1,8 @@
+
+// find index of a element & last index of a number
+
+let nums = [1,20,30,17,89,90,10,27,90,90,5,4,90,33,8];
+
+console.log(nums.indexOf(90));
+
+console.log(nums.lastIndexOf(90));
