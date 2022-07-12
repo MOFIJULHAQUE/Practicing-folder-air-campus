4c6b77dbdb003861e7395874a511018022273518
MofijulHaque <mofijulhoque995@gmail.com>
2022-07-12 16:26:33 +0530

    12-07-2022

---
+++ 12-07-2022/problem_1/index.js
@@ -0,0 +1,7 @@
+// square of element using map
+let nums = [1,2,3,4,5];
+let newArr = nums.map(function(num){
+    console.log(num);
+    return num;
+})
+console.log(nums);
---
+++ 12-07-2022/problem_3/index.js
@@ -0,0 +1,6 @@
+
+// remove all the false value and print truth values
+let nums = [1,'',false,true,'mofijul',null];
+let x = nums.filter(Boolean)
+console.log(x);
+
