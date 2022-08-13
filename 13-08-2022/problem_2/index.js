7ab11d21d8814b2cddc09c1c69369ed43622e649
MofijulHaque <mofijulhoque995@gmail.com>
2022-08-13 18:19:33 +0530

    13-08-2022

---
+++ 13-08-2022/problem_2/index.js
@@ -0,0 +1,12 @@
+// of operator and  for each loop(in array)--------this of operator take the each value and passed inside the loop 
+let nums = [1,2,3,4,5];
+
+for (let num of nums){
+    console.log(num);
+}
+// output
+// 1
+// 2
+// 3
+// 4
+// 5
