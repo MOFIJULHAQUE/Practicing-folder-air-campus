20f9b6a4ba94c40ec3ea9007b78eebea44ef8c98
MofijulHaque <mofijulhoque995@gmail.com>
2022-08-18 19:37:36 +0530

    18-08-2022

---
+++ 18-08-2022/problem_1/index.js
@@ -0,0 +1,41 @@
+
+// print elements in reverse "S" format(revice)
+let matrix = [
+    [1,2,3,4],
+    [11,24,34,44],
+    [17,27,37,474],
+    [11,21,31,41],
+]
+
+
+let rows = matrix.length;
+let columns = matrix[0].length;
+
+for(let i = 0; i < rows; i++){
+    if(i % 2 ==0){
+        for(let j = 0; j < columns; j++){
+            console.log(matrix[i][j]);
+        }
+    }else{
+        for(let j = columns-1;j>=0;j--){
+            console.log(matrix[i][j]);
+        }
+    }
+}
+// output
+// 1
+// 2
+// 3
+// 4
+// 44
+// 34
+// 24
+// 11
+// 17
+// 27
+// 37
+// 474
+// 41
+// 31
+// 21
+// 11
