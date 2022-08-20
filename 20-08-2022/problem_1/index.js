24af6ef308bd937f07d71ef78cae5ffaf11605d6
MofijulHaque <mofijulhoque995@gmail.com>
2022-08-20 19:12:15 +0530

    20-08-2022

---
+++ 20-08-2022/problem_1/index.js
@@ -0,0 +1,8 @@
+// no is start with +91 or not
+let no = '+918638873137';
+
+if(no.startsWith('+91')){
+    console.log('no valid');
+}else{
+    console.log('no invalid');
+}
---
+++ 20-08-2022/problem_2/index.js
@@ -0,0 +1,9 @@
+
+// no is 13 digits or not
+let no = '+918638873137';
+
+if(no.length == 13){
+    console.log('no valid');
+}else{
+    console.log('no invalid');
+}
