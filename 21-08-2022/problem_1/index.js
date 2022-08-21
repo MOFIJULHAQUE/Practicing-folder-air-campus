595e9a15c5a048555e3a3d129f55c0484427167e
MofijulHaque <mofijulhoque995@gmail.com>
2022-08-21 11:28:02 +0530

    21-08-2022

---
+++ 21-08-2022/problem_1/index.js
@@ -0,0 +1,6 @@
+
+// remove 8
+let no = '+918638873137';
+
+console.log(no.replace(/8/g,''));
+
---
+++ 21-08-2022/problem_2/index.js
@@ -0,0 +1,8 @@
+// reverse the no other way 
+let no = '+918638873137';
+
+let reverse = '';
+for(let i=0;i<no.length;i++){
+    reverse = no[i]+ reverse;
+}
+console.log(reverse);
