8b32ae9e73f391f8f2d0529276552354f149eb57
MofijulHaque <mofijulhoque995@gmail.com>
2022-08-25 18:29:13 +0530

    25-08-2022

---
+++ 25-08-2022/problem_1/index.js
@@ -0,0 +1,14 @@
+
+// linear search
+
+let nums = [1,2,3,4,5,6,56,7,8];
+let target  = 56;
+
+function is(nums,target){
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===target) return true;
+    }
+    return false;
+}
+
+console.log(is(nums,target));
