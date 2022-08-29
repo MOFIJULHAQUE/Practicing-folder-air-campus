e50628be6c2cb1e50391b5e934aed9044eb0428c
MofijulHaque <mofijulhoque995@gmail.com>
2022-08-29 09:57:49 +0530

    29-08-2022

---
+++ 29-08-2022/problem_1/index.js
@@ -0,0 +1,13 @@
+// factorial of element using map
+let nums = [1,2,3,4,5];
+
+let factorial = nums.map((num)=>{
+    let fact =1;
+    for(let i=num;i>0;i--){
+        fact*=i
+    }
+    return fact;
+})
+console.log(factorial);
+
+
